1c975f011e8ab470903b0f3befbcecc41dcb1de8
doodhkidhuli <uttarabhagat2002@gmail.com>
2025-05-17 14:23:50 +0530

    PLEASE COMMIT HOJA

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './MyComponent/Header';
+import Todos from './MyComponent/Todos';
+import Footer from './MyComponent/Footer';
+
 function App() {
   return (
     <>
-    <Header/>
-    </>
-    
+    <Header extra="practice" searchbar={true}/>
+    <Todos/>
+    <Footer/>
+  </>
   );
 }
 
